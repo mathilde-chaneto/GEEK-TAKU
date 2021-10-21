f4bb7f24903297e398d9b71a0501b3849786b9c0
mathilde-chaneto <mathilde.chaneto@gmail.com>
2021-10-21 14:04:49 +0200

    modify few files

--- public/js/track.js
+++ public/js/track.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const test = document.getElementById('skills');
-    console.log(test);
+    const skills = document.getElementById('skills');
+    //console.log(test);
 
     const className = "in-view";
     const stopClass = "stopped";
 
-    test.classList.remove(className);
+    skills.classList.remove(className);
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     //test.classList.remove(stopClass);
-                    test.classList.add(className);
-                    test.classList.remove(stopClass);
+                    skills.classList.add(className);
+                    skills.classList.remove(stopClass);
 
                     return;
                 }
-                if (test.classList.contains('stopped')) {
-                    test.classList.remove(className);
-                    test.style.animationName = "none";
+                if (skills.classList.contains('stopped')) {
+                    skills.classList.remove(className);
+                    skills.style.animationName = "none";
                 }
 
-                test.classList.add(stopClass);
+                skills.classList.add(stopClass);
 
 
             });
@@ -32,5 +32,5 @@ document.addEventListener('DOMContentLoaded', () => {
              threshold: 1
          }*/
     );
-    observer.observe(test);
+    observer.observe(skills);
 });
