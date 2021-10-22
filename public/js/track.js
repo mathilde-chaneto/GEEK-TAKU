3840b94fdb0cd5131e8a94f46718aee7c1bb53ac
mathilde-chaneto <mathilde.chaneto@gmail.com>
2021-10-22 13:53:23 +0200

    update css, index template and js

--- public/js/track.js
+++ public/js/track.js
@@ -4,30 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
     //console.log(test);
 
     const className = "in-view";
-    const stopClass = "stopped";
 
+    //by default, remove this class, so the content will not appear
+    // and when we scroll until this element add this class and keep her
+    // the animation is only played one time
     skills.classList.remove(className);
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
-                    //test.classList.remove(stopClass);
+
                     skills.classList.add(className);
-                    skills.classList.remove(stopClass);
+
 
                     return;
                 }
-                if (skills.classList.contains('stopped')) {
-                    skills.classList.remove(className);
-                    skills.style.animationName = "none";
-                }
 
-                skills.classList.add(stopClass);
-
+                //uncomment this line if we want the content is animate every time :
+                // desappear awhen we aren't on it and appear when we are.
+                //skills.classList.remove(className);
 
             });
         },
-        //diplay these elements and after, play animations
+        //diplay these elements and play animations, but it's not what I want
         /* {
              threshold: 1
          }*/
